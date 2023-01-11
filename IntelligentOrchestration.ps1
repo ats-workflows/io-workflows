2e9331ece7c26d8ae9a6a60fa1f5d7d43e4594fe
Neil Pathare <neil.pathare@outlook.com>
2023-01-11 18:02:29 -0500

    Update IntelligentOrchestration.ps1

--- IntelligentOrchestration.ps1
+++ IntelligentOrchestration.ps1
@@ -204,8 +204,8 @@ Function IO_CreateProject() {
   
   $Body = "{`"name`":`"$ProjectName`",`"buildSystem`":`"$BuildSystem`",`"fileChangeThreshold`":$FileChangeThreshold,`"platformVersion`":`"$PlatformVersion`",`"projectLanguage`":`"$ProjectLanguage`",`"projectType`":`"$ProjectType`",`"sensitivePackagePattern`":`"$SensitivePackagePattern`",`"prescanPolicyId`":`"$PreScanPolicyId`",`"postScanPolicyId`":`"$PostScanPolicyId`",`"riskProfilePolicyId`":`"$RiskProfilePolicyId`"}"
 
-  $ProjectResponse = Invoke-RestMethod "$IOURL/api/ioiq/api/portfolio/projects" -Method 'GET' -Headers $Headers -Body $Body
-  $ProjectResponse | CovertTo-Json
+  $ProjectResponse = Invoke-RestMethod "$IOURL/api/ioiq/api/portfolio/projects" -Method 'POST' -Headers $Headers -Body $Body
+  $ProjectResponse | ConvertTo-Json
 
   return $ProjectResponse
 }
