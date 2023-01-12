9ac31e484bedb20625fdc73eb71933e0c2023dec
Neil Pathare <neil.pathare@outlook.com>
2023-01-12 13:45:41 -0500

    Update DevSecOps.ps1

--- DevSecOps.ps1
+++ DevSecOps.ps1
@@ -114,7 +114,7 @@ if (-Not (Test-Path -Path "$IOStateJSON" -PathType Leaf)) {
 } else {
   $PrescriptionJSON = Get-Content 'io_state.json' | Out-String | ConvertFrom-Json -AsHashTable
   $RunId = $PrescriptionJSON.data.io.run.id
-  IO_PrintPrescriptionExplanation = IO_OrchestrationRunDetails $IOURL $IOToken $RunId
+  IO_PrintPrescriptionExplanation $IOURL $IOToken $RunId
 }
 #---------------------------------------------------------------------------------------------------
 
