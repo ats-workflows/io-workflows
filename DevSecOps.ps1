80db3966a36629caf6032e8d6693747ec27ed8d8
Neil Pathare <neil.pathare@outlook.com>
2023-01-12 12:53:40 -0500

    Update DevSecOps.ps1

--- DevSecOps.ps1
+++ DevSecOps.ps1
@@ -17,19 +17,22 @@ Param(
   $ProjectName = "",
   $BranchName = "",
   $ProjectLanguage = "",
+  $OS = "".
   $VerboseOption = "")
 #---------------------------------------------------------------------------------------------------
 
 $VerbosePreference = $VerboseOption
 Get-Date -DisplayHint Date
 Write-Host "Initial directory: $PWD"
+$OriginalPath = Get-Location
 
 # IO Parameters
 $IOWorkflowEngineVersion = "2022.7.1"
 $IOStateJSON = "io_state.json"
 $IOError = "false"
-$IOBaseCommand_Linux = "/opt/synopsys_io/bin/io "
+$IOBaseCommand_Linux = "/home/io "
 $IOBaseCommand_Windows = "io.exe "
+$IOBaseCommand_macOS = "./io "
 #---------------------------------------------------------------------------------------------------
 
 <#
@@ -62,6 +65,7 @@ Write-Host "Project Language: $ProjectLanguage"
 Write-Host "Repository Owner Name: $RepositoryOwner"
 Write-Host "Repository Name: $RepositoryName"
 Write-Host "=========="
+#---------------------------------------------------------------------------------------------------
 
 <#
 ## Intelligent Orchestration - Onboarding
@@ -70,7 +74,48 @@ $IOProject = IO_QueryProjectsByName $IOURL $IOToken $ProjectName
 if ($IOProject -eq $null){
   Write-Host "No project exists by name: $ProjectName"
   $IOProject = IO_CreateProject $IOURL $IOToken $ProjectName $ProjectLanguage
-  Write-Host "Created project by name: $ProjectName with the default properties and policies - a manual edit of the project's configuration is requried."
+  Write-Host "Created project by name: $ProjectName with the default properties and policies - a manual edit of the project's configuration is required."
 } else {
   Write-Host "$ProjectName exists on IO."
 }
+#---------------------------------------------------------------------------------------------------
+
+<#
+## Intelligent Orchestration - Prescription
+## Running: `io --stage io` will produce: "io_state.json" with prescription values for AST.
+## If there is an error or a missing parameter, all AST will be prescribed (values will be set to 'true') for all non-dynamic activities (sast, dast, sca)
+## If there is an error, dynamic activities (cloud/infra/threat-model) will not be added to the state JSON
+#>
+# Set the right base command based on platform
+$IO_StageIO = ""
+$StageIO_Options = "--stage io persona.type='devsecops' io.server.token='$IOToken' io.server.url='$IOURL' project.name='$ProjectName' project.application.name='$ProjectName' "
+$StageIO_SCM = "scm.type='github' scm.owner='$RepositoryOwner' scm.repository.branch.name='$BranchName' scm.repository.name='$RepositoryName' "
+$StageIO_GitHub = "github.apiurl='https://api.github.com/repos' github.ownername='$GitHubUserName' github.repositoryname='$RepositoryName' github.token='$GitHubAccessToken' github.username='$GitHubUserName' "
+
+if ($OS -like "*Linux*") {
+  $IO_StageIO = $IOBaseCommand_Linux + $StageIO_Options + $StageIO_SCM + $StageIO_GitHub
+} elseif ($OS -like "*Windows*") {
+  $IO_StageIO = $IOBaseCommand_Windows + $StageIO_Options + $StageIO_SCM + $StageIO_GitHub
+} elseif ($OS -like "*macOS*") {
+  $IO_StageIO = $IOBaseCommand_macOS + $StageIO_Options + $StageIO_SCM + $StageIO_GitHub
+} else {
+  Write-Error "Unknown build type."
+  Exit
+}
+
+Write-Host "=========="
+Write-Host "IO - Stage IO (Prescription)"
+Write-Host "=========="
+Invoke-Expression $IO_StageIO
+
+# Running stage IO should result in a state/prescription JSON
+if (-Not (Test-Path -Path "$IOStateJSON" -PathType Leaf)) {
+  $IOError = "true"
+} else {
+  $PrescriptionJSON = Get-Content 'io_state.json' | Out-String | ConvertFrom-Json -AsHashTable
+}
+#---------------------------------------------------------------------------------------------------
+
+<#
+## Polaris - Onboarding
+#>
